349d222c0d8225eeac2e629eba5bf794c92814af
Kyle Boucher <kyle.boucher@lancom.co.nz>
2018-09-04 11:56:46 +1200

    init project

---
+++ AutoDeploy/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AutoDeploy.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ AutoDeploy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AutoDeploy.WebApiApplication" Language="C#" %>
