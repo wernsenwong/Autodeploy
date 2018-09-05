c0e2babbb388e50a3b62a1c86fb131565e6907ca
Wernsen Wong <wernsen.wong@lancom.co.nz>
2018-09-06 08:48:25 +1200

    Add tests

---
+++ AutoDeployTests/Controllers/ValuesControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AutoDeploy.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoDeploy.Controllers.Tests {
+    [TestClass()]
+    public class ValuesControllerTests {
+        [TestMethod()]
+        public void GetTestSingle() {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void GetTestList() {
+            Assert.Fail();
+        }
+    }
+}
